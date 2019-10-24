4cffdc890e0a35df4964a81dd96d0d4225282ff0
Jeevi Natarajan <Jeevi.Natarajan@ril.com>
2019-10-24 23:54:56 +0530

    Two sum tested in Leet code

--- src/com/jv/array/TwoSum.java
+++ src/com/jv/array/TwoSum.java
@@ -1,5 +1,42 @@
 package com.jv.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * @author Jeevi.Natarajan
+ * 
+ * https://leetcode.com/problems/two-sum/
+ *
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ */
 public class TwoSum {
 
+	
+	
+	public static int [] twoSum(int [] arr , int x) {
+		Map<Integer,Integer> map = new HashMap<>(); // Have to return the indexes.
+		
+		for(int i=0;i<arr.length;i++) {
+			
+			if(map.containsKey(arr[i])) {
+				int [] res = new int[2];
+				res[0] = i;
+				res[1] = map.get(arr[i]);
+				
+				return res;
+			}
+			
+			map.put(x-arr[i], i);
+		}
+		
+		return null;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
